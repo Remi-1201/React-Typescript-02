10b67cc808b41c3d0234c9ca9399c2d79b517eb9
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-07-16 20:57:46 +0900

    1. カスタムフックを使わないデータ取得例

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import axios from "axios";
 import { Usercard } from "./components/UserCard";
 import "./styles.css";
-
-// propsのuserに情報を渡す
-const user = {
-  id: 1,
-  name: "Usachan",
-  email: "usagi@suki.com",
-  address: "USALAND"
-};
+// types/api/user.tsで指定した配列のUser型をaxiosに渡す
+import { User } from "./types/api/user";
+// useStateを使って、axiosで取得したデータをコンポネントのUserCardに渡す
+import { useState } from "react";
+// types/userProfile.tsで指定した配列のUserProfile型をuseStateに渡す
+import { UserProfile } from "./types/userProfile";
 
 export default function App() {
+  //  UserProfile の配列を useState で定義
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  // loadingのフラグ、初期値 = false
+  const [loading, setLoading] = useState(false);
+  // エラーがあるかどうかを判定するState
+  const [error, setError] = useState(false);
+
   const onClickFetchUser = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users");
+    // clickした時にloadingの状態がtrueになる
+    setLoading(true);
+    setError(false);
+
+    // get<Array<User>> = types/api/user.tsで指定した配列のUser型をaxiosに渡す
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        // APIから取得したデータの各要素(user)を処理する関数
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      // catch() = エラーがある時の処理
+      .catch(() => {
+        setError(true);
+      })
+      // tsconfig.jsonにある"es2015"を"es2018"に変更すると、finally()も使えるようになる
+      .finally(() => {
+        setLoading(false);
+      });
   };
   return (
     <div className="App">
       {/* APIデータを取得するボタン */}
       <button onClick={onClickFetchUser}>Get data</button>
-      <Usercard user={user} />
+      <br />
+      {/* {error ? (エラーがある時) : (エラーがない時)} */}
+      {error ? (
+        <p style={{ color: "red" }}>Some API errors occurred</p>
+      ) : loading ? (
+        <p>Loading ... </p>
+      ) : (
+        // エラーでもloadingでもない場合
+        <>
+          {userProfiles.map((user) => (
+            <Usercard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
--- src/types/api/user.ts
+++ src/types/api/user.ts
@@ -1,1 +1,24 @@
-// APIから取得したデータの型を定義
+// APIから取得したデータの型を定義し、axiosに渡す
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
